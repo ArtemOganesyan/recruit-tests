b089450c36c9030658f9859d554ed10a17a8af8b
Artem Oganesyan <artem.oganesyan@gmail.com>
2024-05-08 17:05:10 -0700

    upd

--- src/test/java/support/TestContext.java
+++ src/test/java/support/TestContext.java
@@ -45,7 +45,7 @@ public class TestContext {
         if (testEnv.equals("local")) {
             switch (browser) {
                 case "chrome":
-                    WebDriverManager.chromedriver().browserVersion("123").setup();
+                    WebDriverManager.chromedriver().setup();
                     Map<String, Object> chromePreferences = new HashMap<>();
                     chromePreferences.put("profile.default_content_settings.geolocation", 2);
                     chromePreferences.put("download.prompt_for_download", false);
